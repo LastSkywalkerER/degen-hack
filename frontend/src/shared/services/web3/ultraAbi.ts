373dee2dc2560601c1ad4f03cef323c4686762a5
LastSkywalker <maksim.drozd@innowise-group.com>
2024-02-18 13:30:50 +0100

    add sales abi

--- frontend/src/shared/services/web3/ultraAbi.ts
+++ frontend/src/shared/services/web3/ultraAbi.ts
@@ -1,4 +1,5 @@
 import aaveAbi from "./aaveAbi.json";
 import ERC20abi from "./ERC20abi.json";
+import saleslAbi from "./saleslAbi.json";
 
-export const ultraAbi = [...aaveAbi, ...ERC20abi];
+export const ultraAbi = [...aaveAbi, ...ERC20abi, ...saleslAbi];
